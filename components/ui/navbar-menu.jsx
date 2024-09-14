58ad77f385d032234ad574e95d3865d6ebcdfc83
Ryuk0010 <mousambachhar2019@gmail.com>
2024-09-14 07:07:52 +0530

    10th commit

--- components/ui/navbar-menu.jsx
+++ components/ui/navbar-menu.jsx
@@ -27,7 +27,7 @@ export const MenuItem = ({
     >
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
+        className="cursor-pointer text-black hover:opacity-[0.9]"
       >
         {item}
       </motion.p>
@@ -42,7 +42,7 @@ export const MenuItem = ({
               <motion.div
                 transition={transition}
                 layoutId="active"
-                className="bg-slate-800 backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
+                className="bg-slate-800 backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] shadow-xl"
               >
                 <motion.div layout className="w-max h-full p-4 bg-slate-500">
                   {children}
@@ -65,7 +65,7 @@ export const Menu = ({
     (<nav
       // resets the state
       onMouseLeave={() => setActive(null)}
-      className="relative rounded-full border border-transparent bg-slate-200 shadow-input flex justify-center space-x-4 px-8 py-6 ">
+      className="relative rounded-full border border-transparent bg-slate-400 shadow-input flex justify-center space-x-4 px-8 py-6 ">
       {children}
     </nav>)
   );
@@ -87,10 +87,10 @@ export const ProductItem = ({
         alt={title}
         className="flex-shrink-0 rounded-md shadow-2xl" />
       <div>
-        <h4 className="text-xl font-bold mb-1 text-white">
+        <h4 className="text-xl font-bold mb-1 text-white ">
           {title}
         </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem]">
+        <p className="text-neutral-700 text-sm max-w-[10rem] ">
           {description}
         </p>
       </div>
@@ -109,7 +109,7 @@ export const HoveredLink = ({
   return (
     (<Link
       {...rest}
-      className="text-white dark:text-neutral-200 hover:text-black ">
+      className="text-white hover:text-black ">
       {children}
     </Link>)
   );
