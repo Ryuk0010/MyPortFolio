4a51676c393ca16a99aab1c711d7fb34765e1073
Ryuk0010 <mousambachhar2019@gmail.com>
2024-09-14 09:01:33 +0530

    17th commit

--- app/(mainPages)/education/page.jsx
+++ app/(mainPages)/education/page.jsx
@@ -9,7 +9,7 @@ export default function Education() {
     return (
       <div className="w-screen h-screen overflow-x-hidden overflow-y-auto bg-slate-950 text-white pt-28">
         <NavbarDemo/>
-        <div className="flex flex-col lg:flex-row lg:space-x-8 p-4">
+        <div className="flex flex-col lg:flex-row lg:space-x-8 p-4 md:-mt-28">
       <div className="flex-1 mb-4 lg:mb-0">
         <GlobeDemo />
       </div>
@@ -17,7 +17,7 @@ export default function Education() {
         <FlipWordsDemo />
       </div>
     </div>
-        <div className="ml-96 pl-56 mt-52 flex justify-center md:pl-0 md:mt-20 md:mb-20">
+        <div className="ml-96 pl-56 mt-52 flex justify-center md:pl-0 md:mt-20 md:mb-20 overflow-hidden ">
         <div className="text-5xl font-bold mr-96 -ml-96">
           Degree
         </div>
@@ -30,7 +30,7 @@ export default function Education() {
       </div>
     </div>
 
-        <div className="ml-96 pl-56 mt-52 flex justify-center md:pl-0 md:mt-20 md:mb-20">
+        <div className="ml-96 pl-56 mt-52 flex justify-center md:pl-0 md:mt-20 md:mb-20 overflow-hidden">
           <div className="text-5xl font-bold mr-96 -ml-96">
             Certificates
           </div>
