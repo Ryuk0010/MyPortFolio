e7d3ceb97cfce1b8ce6405c0e7aa42c07b6b33f6
Ryuk0010 <mousambachhar2019@gmail.com>
2025-04-17 00:30:32 +0530

    photo changed1

--- app/components/Wobble.jsx
+++ app/components/Wobble.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import { WobbleCard } from "@/components/ui/wobble-card";
-import Photo from '@/image/batman.jpg';
+import Photo from '@/image/batman.png';
 
 export function WobbleCardDemo() {
   return (
